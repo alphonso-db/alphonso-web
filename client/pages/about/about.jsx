794e1dc093b6add359b34a5bfaead2ef61084491
hegdeashwin <ashwin.hegde3@gmail.com>
2015-10-23 01:20:38 +0530

    Adds initial dummy dirs

---
+++ client/pages/about/about.jsx
@@ -0,0 +1,17 @@
+(function() {
+	'use strict';
+
+	var React = require('react');
+
+	var About = React.createClass({
+		render: function() {
+			return (
+				<div>
+					<h1>About Page</h1>
+				</div>
+			);
+		}
+	});
+
+	module.exports = About;
+})();
---
+++ client/pages/settings/settings.jsx
@@ -0,0 +1,17 @@
+(function() {
+	'use strict';
+
+	var React = require('react');
+
+	var Settings = React.createClass({
+		render: function() {
+			return (
+				<div>
+					<h1>Settings Page</h1>
+				</div>
+			);
+		}
+	});
+
+	module.exports = Settings;
+})();
