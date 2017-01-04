ed8534b780b37dac9a1215ac4de0c7cb62491052
hegdeashwin <ashwin.hegde3@gmail.com>
2017-01-04 23:31:38 +0530

    Removes static files

---
+++ dist/alphonso.js
@@ -0,0 +1,5 @@
+"use strict";
+var ReactDOM = require("react-dom");
+var router_1 = require("./router/router");
+ReactDOM.render(router_1.default, document.getElementById("root-container"));
+//# sourceMappingURL=alphonso.js.map
